8fc9088aec845dc4e2248937d9ad58f5995cd00c
matfax <matthias.fax@gmail.com>
2016-12-17 20:34:44 +0100

    Release version: 1.0.1

--- src/test/scala/com/matfax/swuiakht/SwaggerUiRouteSpec.scala
+++ src/test/scala/com/matfax/swuiakht/SwaggerUiRouteSpec.scala
@@ -11,7 +11,7 @@ import scala.io.{Codec, Source}
   * The specification for ``SwaggerUiRoute``.
   *
   * @author Matthias Fax
-  * @version 1.0.0
+  * @version 1.0.1
   */
 class SwaggerUiRouteSpec extends WordSpec with Matchers with ScalatestRouteTest {
 
